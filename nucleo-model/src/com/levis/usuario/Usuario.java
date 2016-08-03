e75cb3c09ff82846fc70daf7366b3e168eab5949
levipegoretti <levipegoretti@gmail.com>
2016-08-03 20:19:41 -0300

    criacao da classe usuario

---
+++ nucleo-model/src/com/levis/usuario/Usuario.java
@@ -0,0 +1,31 @@
+package com.levis.usuario;
+
+import java.io.Serializable;
+import java.lang.Long;
+import javax.persistence.*;
+
+/**
+ * Entity implementation class for Entity: Usuario
+ *
+ */
+@Entity
+
+public class Usuario implements Serializable {
+
+	   
+	@Id
+	private Long id;
+	private static final long serialVersionUID = 1L;
+
+	public Usuario() {
+		super();
+	}   
+	public Long getId() {
+		return this.id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+   
+}
